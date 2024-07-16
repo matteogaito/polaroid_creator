3c733ef889b1dd48acf2f20b44501fb005cc84b1
Matteo Gaito <m@vugifarm.com>
2024-07-16 21:04:59 +0200

    added wide and portrait polaroid

--- polaroid_creator.py
+++ polaroid_creator.py
@@ -47,17 +47,49 @@ def createSquare(filename):
     square_pic.save('squares/square_' + infos['filename'] + infos['extension'])
     return(square_pic)
 
-def resize_image( image, base, height ):
+def createImage(filename):
+    infos = GetImageInfos(filename)
+    original_im = Image.open(filename)
+    return(original_im)
+
+def resize_image( image, base=None, height=None ):
     orig_base, orig_height = image.size
-    if orig_base == base and orig_height == height:
-        logging.info('Img not resized')
-        return(image)
-    logging.info('Resizing img')
-    return(image.resize((base,height), Image.ANTIALIAS))
+    
+    if base is None and height is None:
+        logging.error('At least one of base or height must be specified')
+        return image
+    
+    if base and height:
+        if orig_base == base and orig_height == height:
+            logging.info('Resizing img')
+            return(image.resize((base,height), Image.Resampling.LANCZOS))
+
+    if base:
+        new_base = base
+        new_height = int((new_base / orig_base) * orig_height)
+    else:
+        new_height = height
+        new_base = int((new_height / orig_height) * orig_base)
+
+    logging.info('Resizing img with calculated aspect ratio')
+    return(image.resize((new_base,new_height), Image.Resampling.LANCZOS))
+
+def get_biggest_sizes(images_array):
+    h = []
+    w = []
+    biggests = []
+    for image in images_array:
+        w.append(image.size[0])
+        h.append(image.size[1])
+
+    biggests.append(max(w))
+    biggests.append(max(h))
+    return(biggests)
+
 
 def createStrip(images_array, strip_name, delimiter=False):
     num_elements = len(images_array)
-    onesize = images_array[0].size[0]
+    w, h = get_biggest_sizes(images_array=images_array) 
 
     if delimiter:
         line = 5
@@ -65,13 +97,13 @@ def createStrip(images_array, strip_name, delimiter=False):
     else:
         line = 0
 
-    strip_size = ( ( ( onesize * num_elements) + (line * 3)), ( onesize + line ) )
+    strip_size = ( ( ( w * num_elements) + (line * 3)), ( h + line ) )
     strip = Image.new("RGB", strip_size, color=("Black"))
     goforward = 0
     for image in images_array:
         logging.info("image base {} height {}".format(image.size[0],image.size[1]))
         strip.paste(image, (goforward + line, line))
-        goforward = goforward + onesize + line
+        goforward = goforward + w + line
 
     logging.info('saving strip {}'.format(strip_name))
     strip.save('output/' + strip_name)
@@ -89,6 +121,8 @@ parser.add_option("-d", "--directory", dest="src_dir", default=False, type="stri
 parser.add_option("-r", "--row", dest="row_num", type="int", default=2, help="Number of element in a row")
 parser.add_option("-D", "--delimiter", dest="delimiter", default=True, action='store_true', help="Show delimiter")
 parser.add_option("-t", "--add-text", dest="add_text", default=False, help="Text to add to bottom to the polaroid")
+parser.add_option("--use-filename-astext", dest="add_filenametext", default=False, action='store_true', help="Use the filename as text")
+parser.add_option("--as_square", dest="as_square", default=False, action='store_true', help="cut to have square" )
 (options, args) = parser.parse_args()
 
 if options.src_dir == False:
@@ -100,51 +134,81 @@ logging.info("Source Directory: {}, Element in row: {}, Delimiter: {}, Text: {}"
 # Get all images
 for filename in glob.glob(options.src_dir + '/*.*'):
     photo_path = Path(filename)
+    logging.info("get {}".format(filename))
     file_ext = ((photo_path.suffix).upper()).replace(".","")
+    filename_as_text = photo_path.stem.split('_')[0]
     if file_ext in img_exts:
-        squares.append(createSquare(filename))
+        square = {}
+        square['text'] = filename_as_text
+        if options.as_square:
+            square['img'] = createSquare(filename)
+        else:
+            square['img'] = createImage(filename) 
+        squares.append(square)
 
-# Define biggest images square size
-img_size_array = []
+### Getting size of images
+images_array = []
 for image in squares:
-    img_size_array.append(image.size[0])
+    images_array.append(image['img'])
 
-biggest_img = max(img_size_array)
-logging.info('Biggest size is {}'.format(str(biggest_img)))
+biggest_w, biggest_h = get_biggest_sizes(images_array=images_array)
+logging.info('Biggest size w is {}'.format(str(biggest_w)))
+logging.info('Biggest size h is {}'.format(str(biggest_h)))
 
 # Create squares with same size
 for idx, image in enumerate(squares):
-    if biggest_img != image.size[0]:
-        squares[idx] = resize_image( image, biggest_img, biggest_img )
+    if biggest_w != image['img'].size[0]:
+        if options.as_square:
+            squares[idx]['img'] = resize_image( image['img'], biggest_w, biggest_w) 
+        else:
+            squares[idx]['img'] = resize_image( image['img'], biggest_w) 
 
 # adding white border
-border = int( ( biggest_img * 11 ) / 100 )
+border = int( ( biggest_w * 11 ) / 100 )
 for idx, image in enumerate(squares):
-    polaroid = ImageOps.expand(image, border=border, fill='white')
-    squares[idx] = polaroid
+    polaroid = ImageOps.expand(image['img'], border=border, fill='white')
+    squares[idx]['img'] = polaroid
 
 # adding text
 if options.add_text != False:
     logging.info("Drawing text '{}' to fotos".format(options.add_text))
     for idx, image in enumerate(squares):
-        editing_polaroid = ImageDraw.Draw(image)
-        related_font_size = int(border / 1.9)
-        font = ImageFont.truetype('fonts/Laila-Medium.ttf', size=related_font_size)
-        w_text, h_text = font.getsize(options.add_text)
+        editing_polaroid = ImageDraw.Draw(image['img'])
+        related_font_size = int(border / 2.0)
+        font = ImageFont.truetype('fonts/RockSalt-Regular.ttf', size=related_font_size)
+        bbox = editing_polaroid.textbbox((0, 0), options.add_text, font=font)
+        w_text = bbox[2] - bbox[0]
+        h_text = bbox[3] - bbox[1]
         center_h =  ( image.size[1] - ( h_text / 2 ) ) - ( border / 2 )
         center_w = ( image.size[0] - w_text ) / 2
         editing_polaroid.text( ( center_w, center_h ), options.add_text, fill="black", font=font)
+
+if options.add_filenametext:
+    for idx, image in enumerate(squares):
+        logging.info("Drawing filename text {}".format(image['text']))
+        editing_polaroid = ImageDraw.Draw(image['img'])
+        related_font_size = int(border / 3.0)
+        font = ImageFont.truetype('fonts/RockSalt-Regular.ttf', size=related_font_size)
+        bbox = editing_polaroid.textbbox((0, 0), image['text'], font=font)
+        w_text = bbox[2] - bbox[0]
+        h_text = bbox[3] - bbox[1]
+        center_h =  ( image['img'].size[1] - ( h_text / 2 ) ) - ( border / 1.5 )
+        center_w = ( image['img'].size[0] - w_text ) / 2
+        editing_polaroid.text( ( center_w, center_h ), image['text'], fill="black", font=font)
 
 # Create strip
 line = 5
 strip_num = 1
 images_for_strip = []
 
+print(squares)
+
 logging.info("create strip with {} images".format(options.row_num))
 for idx, image in enumerate(squares):
     logging.info("appending image for strip")
-    images_for_strip.append(image)
+    images_for_strip.append(image['img'])
     if len(images_for_strip) == options.row_num:
+        logging.info("creating strip...")
         strip_name = 'strip_' + str(strip_num) + '.jpg'
         createStrip(images_for_strip, strip_name, options.delimiter)
         images_for_strip = []
